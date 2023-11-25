cbcaaa939e2c163e68c1632b6d6bbb211d1fb4d9
Knstvsl <7770hunter0777@gmail.com>
2023-11-26 00:01:52 +0800

    [TSK-140] test failure check

--- user-manager/src/test/java/Test.java
+++ user-manager/src/test/java/Test.java
@@ -3,6 +3,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class Test {
     @org.junit.jupiter.api.Test
     void test() {
-        assertThat(true).isTrue();
+        assertThat(false).isTrue();
     }
 }

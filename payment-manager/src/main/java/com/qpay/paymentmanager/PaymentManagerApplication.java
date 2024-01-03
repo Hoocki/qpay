5be907fcc47b3e0a2da7e0d802b9657eb74aa98c
kostya420 <75415127+kostya4201337@users.noreply.github.com>
2024-01-03 20:56:43 +0800

    [TSK-178] init payment manager (#13)
    [TSK-178] init payment manager

---
+++ payment-manager/src/main/java/com/qpay/paymentmanager/PaymentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.qpay.paymentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(PaymentManagerApplication.class, args);
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "qpay"
 
 include("user-manager")
+include("payment-manager")

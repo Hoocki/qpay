dcb486bf5d7d5585d459fbf957ea54c08d3dbd3f
Konstantin <vaskon0306@gmail.com>
2024-01-08 15:35:43 +0800

    [TSK-184] add getWalletByUser method

--- payment-manager/src/main/java/com/qpay/paymentmanager/service/WalletService.java
+++ payment-manager/src/main/java/com/qpay/paymentmanager/service/WalletService.java
@@ -1,5 +1,6 @@
 package com.qpay.paymentmanager.service;
 
+import com.qpay.libs.models.UserType;
 import com.qpay.paymentmanager.model.dto.WalletCreation;
 import com.qpay.paymentmanager.model.dto.WalletModification;
 import com.qpay.paymentmanager.model.entity.WalletEntity;
@@ -7,6 +8,8 @@ import com.qpay.paymentmanager.model.entity.WalletEntity;
 public interface WalletService {
 
     WalletEntity getWalletById(long id);
+
+    WalletEntity getWalletByUser(long userId, UserType userType);
 
     WalletEntity addWallet(WalletCreation walletModification);
 

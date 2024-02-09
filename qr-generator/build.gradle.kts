74bfff539b2da1ed982320056b6729103a201cda
Konstantin <vaskon0306@gmail.com>
2024-02-09 23:40:56 +0800

    [TSK-177] init qr generator

---
+++ qr-generator/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ rootProject.name = "qpay"
 
 include("user-manager")
 include("payment-manager")
+include("qr-generator")
 include(":libs")

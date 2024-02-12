010f325856666a6bcffd9852bcd393ba52004367
Konstantin <vaskon0306@gmail.com>
2024-02-12 20:14:16 +0800

    [TSK-177] rename example folder to qpay

--- qr-generator/src/main/java/com/qpay/qrgenerator/QrGeneratorApplication.java
+++ qr-generator/src/main/java/com/qpay/qrgenerator/QrGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.example.qrgenerator;
+package com.qpay.qrgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

f70f6e3ebe573bf61e58be0ef93ae5ab83c4e270
Konstantin <vaskon0306@gmail.com>
2024-02-17 16:55:40 +0800

    [TSK-203] move plugins versions to root

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,8 @@ allprojects {
 		set("zxingJavaSe", "3.5.3")
 	}
 }
+
+plugins {
+	id("org.springframework.boot") version "3.2.2" apply false
+	id("io.spring.dependency-management") version "1.1.4" apply false
+}
--- payment-manager/build.gradle.kts
+++ payment-manager/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.2"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 java {

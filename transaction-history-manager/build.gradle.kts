3276957d0cfd284cb78712bc69d7ff8cf12bb271
kostya420 <75415127+kostya4201337@users.noreply.github.com>
2024-02-22 23:52:34 +0800

    [TSK-216] init transaction history manager (#37)

--- transaction-history-manager/build.gradle.kts
+++ transaction-history-manager/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
 }
 
 dependencies {
+    annotationProcessor("org.projectlombok:lombok")
+
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    compileOnly("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

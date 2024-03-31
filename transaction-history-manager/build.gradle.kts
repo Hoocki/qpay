f0699238b68f9d52e242937495a12536a9e9b93f
Konstantin <vaskon0306@gmail.com>
2024-03-31 13:01:51 +0800

    [TSK-226] add Authentication filter in api gateway, remove duplicated dependencies

--- transaction-history-manager/build.gradle.kts
+++ transaction-history-manager/build.gradle.kts
@@ -4,17 +4,11 @@ plugins {
 
 dependencies {
     implementation(project(":libs"))
-    annotationProcessor("org.projectlombok:lombok")
-
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.itextpdf:itext7-core:${Versions.I_TEXT_PDF}")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:${Versions.OPENFEIGN}")
 
     testImplementation("org.wiremock:wiremock-standalone:${Versions.WIREMOCK}")
-
-    compileOnly("org.projectlombok:lombok")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
